2d63262f036af761145ecd946041ea011b8c3618
Howard Wu <howardwu20@outlook.com>
2025-09-14 00:00:36 +0800

    Add hooks for RescueParty and DeviceConfig access

--- SystemDebug/src/main/java/io/github/howard20181/systemservicedebug/DebugHook.java
+++ SystemDebug/src/main/java/io/github/howard20181/systemservicedebug/DebugHook.java
@@ -1,13 +1,16 @@
 package io.github.howard20181.systemservicedebug;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.VersionedPackage;
+import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import java.util.List;
+import java.util.concurrent.Executor;
 
 import io.github.libxposed.api.XposedInterface;
 import io.github.libxposed.api.XposedModule;
@@ -25,16 +28,43 @@ public class DebugHook extends XposedModule {
     }
 
     @Override
+    public void onPackageLoaded(@NonNull PackageLoadedParam param) {
+        var classLoader = param.getClassLoader();
+        var pn = param.getPackageName();
+        try {
+            log("Loaded package: " + pn);
+//            hookProviderSettings(classLoader);
+        } catch (Throwable tr) {
+            log("Error hooking system framework", tr);
+        }
+    }
+
+    @Override
     public void onSystemServerLoaded(@NonNull SystemServerLoadedParam param) {
         var classLoader = param.getClassLoader();
         try {
 //            hookRescuePartyPlusHelper(classLoader);
             hookOnHealthCheckFailed(classLoader);
             hookPackageWatchdogImpl(classLoader);
-//            hookPackageWatchdog(classLoader);
+            hookPackageWatchdog(classLoader);
+            hookRescuePartyMonitorCallback(classLoader);
         } catch (Throwable tr) {
             log("Error hooking system service", tr);
         }
+    }
+
+    private void hookRescuePartyMonitorCallback(ClassLoader classLoader) throws NoSuchMethodException, ClassNotFoundException {
+        var aClass = classLoader.loadClass("com.android.server.RescueParty$RescuePartyMonitorCallback");
+        var method = aClass.getDeclaredMethod("onDeviceConfigAccess", String.class, String.class);
+        hook(method, DumpStackHooker.class);
+        hook(method, OnDeviceConfigAccessHooker.class);
+    }
+
+    private void hookProviderSettings(ClassLoader classLoader) throws NoSuchMethodException, ClassNotFoundException {
+        var aClass = classLoader.loadClass("android.provider.Settings$Config");
+        var innClass = classLoader.loadClass("android.provider.DeviceConfig$MonitorCallback");
+        var method2 = aClass.getDeclaredMethod("setMonitorCallback", ContentResolver.class, Executor.class, innClass);
+        hook(method2, DumpStackHooker.class);
     }
 
     private void hookPackageWatchdog(ClassLoader classLoader) throws NoSuchMethodException, ClassNotFoundException {
@@ -59,7 +89,7 @@ public class DebugHook extends XposedModule {
         var method = aClass.getDeclaredMethod("onHealthCheckFailed", VersionedPackage.class, int.class, int.class);
         var method2 = bClass.getDeclaredMethod("onHealthCheckFailed", VersionedPackage.class, int.class, int.class);
         hook(method, OnHealthCheckFailedHooker.class);
-//        hook(method2, DumpStackHooker.class);
+        hook(method2, OnHealthCheckFailedHooker.class);
     }
 
     private void hookRescuePartyPlusHelper(ClassLoader classLoader) throws NoSuchMethodException, ClassNotFoundException {
@@ -76,8 +106,7 @@ public class DebugHook extends XposedModule {
             var failedPackage = (VersionedPackage) args[0];
             var failureReason = (int) args[1];
             var mitigationCount = (int) args[2];
-            var pClass = callback.getThisObject();
-            var className = pClass != null ? pClass.getClass().getSimpleName() : "RescueParty";
+            var className = callback.getMember().getDeclaringClass().getSimpleName();
             Log.d(className, "onHealthCheckFailed failedPackage=" + failedPackage + ", failureReason=" + failureReason + ", mitigationCount=" + mitigationCount + " return=" + callback.getResult());
         }
     }
@@ -103,6 +132,18 @@ public class DebugHook extends XposedModule {
             var crashMember = (int) args[1];
 //            var context = (Context) args[3];
             Log.d("PackageWatchdogImpl", "setNowCrashApplicationLevel failedPackage=" + failedPackage + ", crashMember=" + crashMember + ", mitigationCount=" + mitigationCount + " return=" + callback.getResult());
+        }
+    }
+
+    @XposedHooker
+    private static class OnDeviceConfigAccessHooker implements Hooker {
+        @AfterInvocation
+        public static void after(@NonNull AfterHookCallback callback) {
+            var args = callback.getArgs();
+            var callingPackage = (String) args[0];
+            var namespace = (String) args[1];
+            var className = callback.getMember().getDeclaringClass().getSimpleName();
+            Log.d(className, callback.getMember().getName() + " callingPackage=" + callingPackage + ", namespace=" + namespace);
         }
     }
 }
